2e11ae019559f56ba98e238f931b3bfb67209c6c
y-prog <ydg886@gmail.com>
2024-11-21 13:36:04 +0100

    structure

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MenuWithDatabase;
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        Database database = new();
+        var db = database.Connection();
+        var actions = new Actions(db);
+        new Menu(actions);
+    }
+}
